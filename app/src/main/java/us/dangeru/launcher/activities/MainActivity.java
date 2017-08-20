b7d5e7e680b55012dcd0fb390eda16a79944d5e7
Niles Rogoff <me@niles.xyz>
2017-08-20 16:54:10 -0700

    some more comments

--- app/src/main/java/us/dangeru/launcher/activities/MainActivity.java
+++ app/src/main/java/us/dangeru/launcher/activities/MainActivity.java
@@ -25,17 +25,21 @@ public class MainActivity extends Activity implements UnitedActivity {
     private UnitedWebFragment webFragment;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Register to receive reloads to music.html later on
         ReloadService.register(this);
         setContentView(R.layout.main_activity);
-        FragmentManager manager = getFragmentManager();
+        // load and retrieve session variables
         if (savedInstanceState != null) {
             session = ParcelableMap.fromParcel(savedInstanceState.getParcelable("session"));
         } else {
             session = new ParcelableMap();
         }
+        // put our UnitedWebFragment on there
+        FragmentManager manager = getFragmentManager();
         webFragment = (UnitedWebFragment) manager.findFragmentByTag("main_webkit_wrapper");
         if (webFragment == null) {
             webFragment = new UnitedWebFragment();
+            // if we have a URL in the intent, load that, otherwise load index.html
             Bundle args = new Bundle();
             if (getIntent() != null && getIntent().hasExtra("URL")) {
                 args.putString("URL", getIntent().getStringExtra("URL"));
@@ -53,9 +57,11 @@ public class MainActivity extends Activity implements UnitedActivity {
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+        // save session variables to the instance state
         outState.putParcelable("session", session.parcel());
     }
 
+    // launches a HTML page in a new activity
     @Override
     public void launchHTML(String resource) {
         Intent i = new Intent(this, MainActivity.class);
@@ -63,9 +69,12 @@ public class MainActivity extends Activity implements UnitedActivity {
         extras.putString("URL", resource);
         extras.putBoolean("support_back_button", true);
         i.putExtras(extras);
+        // get notified when it exits
         startActivityForResult(i, 0);
     }
     @Override protected void onActivityResult(int request_code, int result_code, Intent bundle) {
+        // if the page we opened requested that we reload, do so.
+        // currently used by camo_customize.html to reload the home screen because the theme has changed
         if (result_code == 1) {
             try {
                 getWebView().reload();
@@ -89,6 +98,7 @@ public class MainActivity extends Activity implements UnitedActivity {
         session.put(key, value);
     }
 
+    // finishes the current activity, requesting that the opener refresh the page if needed
     @Override
     public void closeWindow(boolean refresh) {
         if (refresh) {
@@ -99,16 +109,20 @@ public class MainActivity extends Activity implements UnitedActivity {
         finish();
     }
 
+    // since UnitedActivity can't extend Activity, if you want to call Activity methods on one (getApplication, etc..) you need to chain this
     @Override
     public Activity asActivity() {
         return this;
     }
 
+    // Gets the webview from the view, used in ReloadService for reloading
     @Override
     public WebView getWebView() {
         return findViewById(R.id.main_webkit);
     }
 
+    // If support_back_button is unset or false, tries to go back in the webview, and if there's no more history, will finish the activity
+    // if support_back_button is true (it never is), just finish immediately
     @Override
     public void onBackPressed() {
         if (getIntent() != null && getIntent().hasExtra("support_back_button") && getIntent().getBooleanExtra("support_back_button", false)) {
@@ -123,6 +137,8 @@ public class MainActivity extends Activity implements UnitedActivity {
         }
     }
 
+    // play a dumb sound when closing an activity
+    // only there because the original app had it
     @Override
     public void finish() {
         new Thread() {

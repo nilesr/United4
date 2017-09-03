0972f05726bdb229ee4601cf717d14bc437adb5a
Niles Rogoff <me@niles.xyz>
2017-09-03 16:47:41 -0400

    Add classy theme

--- app/src/main/res/raw/mock.js
+++ app/src/main/res/raw/mock.js
@@ -54,7 +54,7 @@ if (typeof(window.unitedPropertiesIf) == "undefined") {
 		getProperty: function(name) {
 			if (name == "ordered_songs") return "[]";// JSON.stringify(['Hopes and Dreams', 'A Neon Glow Lights the Way', 'Welcome To VA-11 HALL-A', 'Every Day is Night', 'Commencing Simulation', 'Drive Me Wild', 'Good for Health, Bad for Education', 'Who Was I?', 'Troubling News', 'A Gaze That Invited Disaster', 'Friendly Conversation', "You've Got Me", 'Umemoto', "JC Elton's", 'Go! Go! Streaming-chan!', 'All Systems, Go!', 'Where Do I Go From Here?', 'Will You Remember Me?', 'Everything Will Be Okay', 'March of the White Knights', 'A. Rene', 'Neo Avatar', 'Those Who Dwell in The Shadows', 'Nighttime Manuvers', 'A Star Pierces the Darkness', 'Your Love is a Drug', 'Through the Storm We Will Find a Way', 'Synthestitch', 'Snowfall', 'The Answer Lies Within', 'Dawn Approaches', 'With Renewed Hope, We Continue Forward', 'Last Call', 'Reminiscence', 'Believe in Me Who Believes in You', 'Final Result', 'Until We Meet Again', 'Digital Drive', 'Safe Haven']);
 			//if (name == "all_themes") return JSON.stringify(["normal", "dotted", "steam", "kira", "meme", "vaporwave"]);
-			if (name == "all_themes") return JSON.stringify(["normal", "vaporwave", "noir", "burg", "unity", "empire"]);
+			if (name == "all_themes") return JSON.stringify(["normal", "vaporwave", "noir", "burg", "unity", "empire", "classy"]);
 			if (name == "version_notes") return "Version 4.1.3!\nTap for Patch Notes"
 			if (name == "awoo_endpoint") return "https://dangeru.us";
 			var res = localStorage.getItem(name);

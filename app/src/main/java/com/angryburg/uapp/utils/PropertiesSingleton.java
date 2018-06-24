85e58ad1965ca97895ad0197fda1e4a26f4c75fc
Niles Rogoff <me@niles.xyz>
2018-06-24 13:38:41 +0000

    oops

--- app/src/main/java/com/angryburg/uapp/utils/PropertiesSingleton.java
+++ app/src/main/java/com/angryburg/uapp/utils/PropertiesSingleton.java
@@ -125,7 +125,7 @@ public final class PropertiesSingleton {
         setProperty("is_playing", "false");
         //List<String> themes = Arrays.asList("normal", "dotted", "steam", "kira", "meme", "vaporwave");
         // rip the meme theme, it was my favorite
-        List<String> themes = Arrays.asList("normal", "vaporwave", "noir", "burg", "unity", "empire", "classy", "lain", "neon");
+        List<String> themes = Arrays.asList("normal", "vaporwave", "noir", "burg", "unity", "empire", "classy", "lain", "neon", "kids", "motif");
         String str = new JSONArray(themes).toString();
         setProperty("all_themes", str);
         HashMap<String, String> map = new HashMap<>();
